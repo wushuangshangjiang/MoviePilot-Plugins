40eed9b68ef4e3b1a9d9fe5a9be69b410d82f2bf
wushuangshangjiang <139621456+wushuangshangjiang@users.noreply.github.com>
2024-07-20 08:58:35 +0800

    Update __init__.py

--- plugins/webhook/__init__.py
+++ plugins/webhook/__init__.py
@@ -10,7 +10,7 @@ class WebHook(_PluginBase):
     # 插件名称
     plugin_name = "Webhook-RefreshMedia"
     # 插件描述
-    plugin_desc = "转移完成时,发送webhook,修改字原作者:https://github.com/jxxghp"
+    plugin_desc = "转移完成时,发送webhook,修改自原作者:https://github.com/jxxghp"
     # 插件图标
     plugin_icon = "webhook.png"
     # 插件版本
