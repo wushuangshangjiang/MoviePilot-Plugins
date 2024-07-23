2df8161aef965d977eb2ce35f84db1a4052e7362
wushuangshangjiang <emeanshahabul8386837@gmail.com>
2024-07-23 19:00:22 +0800

    Update __init__.py

--- plugins/rsssubscribe/__init__.py
+++ plugins/rsssubscribe/__init__.py
@@ -33,7 +33,7 @@ class RssSubscribe(_PluginBase):
     # 插件图标
     plugin_icon = "rss.png"
     # 插件版本
-    plugin_version = "1.7"
+    plugin_version = "1.71"
     # 插件作者
     plugin_author = "wushuangshangjiang"
     # 作者主页
