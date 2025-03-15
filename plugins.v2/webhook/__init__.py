f28f41b5f2d6bff699150f51747dfe6b457dbfa8
wushuangshangjiang <139621456+wushuangshangjiang@users.noreply.github.com>
2025-03-15 12:25:26 +0800

    Update __init__.py

--- plugins.v2/webhook/__init__.py
+++ plugins.v2/webhook/__init__.py
@@ -20,7 +20,7 @@ class WebHook(_PluginBase):
     # 作者主页
     author_url = "https://github.com/wushuangshangjiang"
     # 插件配置项ID前缀
-    plugin_config_prefix = "webhook_"
+    plugin_config_prefix = "webhook-refreshmedia_"
     # 加载顺序
     plugin_order = 14
     # 可使用的用户级别
