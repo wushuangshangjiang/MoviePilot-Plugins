743a4f7a9fea694c6b9b665ad1b3762e78d3b663
A Pan <emeanshahabul8386837@gmail.com>
2025-08-20 23:50:39 +0800

    update

--- plugins.v2/iso/__init__.py
+++ plugins.v2/iso/__init__.py
@@ -13,7 +13,7 @@ class ISO(_PluginBase):
     plugin_name = "ISO原盘匹配"
     plugin_desc = "将ISO文件匹配到原盘电影目录"
     plugin_icon = "directory.png"
-    plugin_version = "2.1"
+    plugin_version = "2.2"
     plugin_author = "wushuangshangjiang"
     author_url = "https://github.com/wushuangshangjiang"
     plugin_config_prefix = "iso_"
