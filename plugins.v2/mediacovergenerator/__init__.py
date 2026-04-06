86c816ab163dace13e7e34c0eebe0b48e7389f63
A Pan <laniw4184@gmail.com>
2026-04-07 01:40:59 +0800

    chore: update plugin metadata links

--- plugins.v2/mediacovergenerator/__init__.py
+++ plugins.v2/mediacovergenerator/__init__.py
@@ -53,13 +53,13 @@ class MediaCoverGenerator(_PluginBase):
     # 插件描述
     plugin_desc = "生成媒体库动态/静态封面，支持 Emby/Jellyfin"
     # 插件图标
-    plugin_icon = "https://raw.githubusercontent.com/justzerock/MoviePilot-Plugins/main/icons/emby.png"
+    plugin_icon = "https://raw.githubusercontent.com/wushuangshangjiang/MoviePilot-Plugins/main/icons/emby.png"
     # 插件版本
     plugin_version = "1.0"
     # 插件作者
     plugin_author = "wushuangshangjiang"
     # 作者主页
-    author_url = "https://github.com/justzerock/MoviePilot-Plugins"
+    author_url = "https://github.com/wushuangshangjiang/MoviePilot-Plugins"
     # 插件配置项ID前缀
     plugin_config_prefix = "mediacovergenerator_"
     # 加载顺序
