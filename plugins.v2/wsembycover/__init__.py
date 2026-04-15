ffe4cc1c011ec8752aa7a57d63e642841b2f4b55
wushuangshangjiang <wushuangshangjiang@users.noreply.github.com>
2026-04-15 16:33:38 +0800

    chore(wsembycover): bump version to 1.64

--- plugins.v2/wsembycover/__init__.py
+++ plugins.v2/wsembycover/__init__.py
@@ -77,7 +77,7 @@ class WsEmbyCover(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/wushuangshangjiang/MoviePilot-Plugins/main/icons/emby.png"
     # 插件版本
-    plugin_version = "1.63"
+    plugin_version = "1.64"
     # 插件作者
     plugin_author = "wushuangshangjiang"
     # 作者主页
